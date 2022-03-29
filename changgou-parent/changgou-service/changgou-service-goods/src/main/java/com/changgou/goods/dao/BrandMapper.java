bdf4e6d5a3418e1a98b85dfaf0c17f3997c74de6
CHCIS <CHCIS@qq.com>
2022-03-29 22:57:42 +0800

    代码自动生成

---
+++ changgou-parent/changgou-service/changgou-service-goods/src/main/java/com/changgou/goods/dao/BrandMapper.java
@@ -0,0 +1,18 @@
+package com.changgou.goods.dao;
+
+import com.changgou.goods.pojo.Brand;
+import org.apache.ibatis.annotations.Param;
+import org.apache.ibatis.annotations.Select;
+import tk.mybatis.mapper.common.Mapper;
+
+import java.util.List;
+import java.util.Map;
+
+public interface BrandMapper extends Mapper<Brand> {
+
+    @Select("SELECT name,image FROM tb_brand where id in( SELECT brand_id FROM tb_category_brand WHERE category_id in ( SELECT id from tb_category where name=#{categoryName}))")
+    public List<Map> findBrandListByCategoryName(@Param("categoryName")String categoryName);
+
+    @Select("SELECT tb.* FROM tb_brand tb, tb_category_brand tcb WHERE tb.id=tcb.brand_id AND tcb.category_id=#{categoryId}")
+    public List<Brand> findByCategory(Integer categoryId);
+}
---
+++ changgou-parent/changgou-service/changgou-service-goods/src/main/java/com/changgou/goods/dao/CategoryMapper.java
@@ -0,0 +1,11 @@
+package com.changgou.goods.dao;
+
+import com.changgou.goods.pojo.Category;
+import org.apache.ibatis.annotations.Select;
+import tk.mybatis.mapper.common.Mapper;
+
+import java.util.List;
+
+public interface CategoryMapper extends Mapper<Category> {
+
+}
