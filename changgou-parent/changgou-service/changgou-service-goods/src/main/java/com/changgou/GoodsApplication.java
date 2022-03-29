4fb05adfa9e58d3d53942e200a7952f6b68e97e2
CHCIS <CHCIS@qq.com>
2022-03-29 22:52:29 +0800

    代码自动生成

--- changgou-parent/changgou-service/changgou-service-goods/src/main/java/com/changgou/GoodsApplication.java
+++ changgou-parent/changgou-service/changgou-service-goods/src/main/java/com/changgou/GoodsApplication.java
@@ -1,16 +1,23 @@
 package com.changgou;
 
+import entity.IdWorker;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.context.annotation.Bean;
 import tk.mybatis.spring.annotation.MapperScan;
 
 @SpringBootApplication
 @EnableDiscoveryClient
-@MapperScan(basePackages = {"com.changgou.dao"})
+@MapperScan(basePackages = {"com.changgou.goods.dao"})//开启通用Mapper的包扫描
 public class GoodsApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(GoodsApplication.class,args);
+    }
+
+    @Bean
+    public IdWorker idWorker() {
+        return new IdWorker(0,0);
     }
 }
